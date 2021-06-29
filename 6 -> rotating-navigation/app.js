71901944cd7d1bd143906b9de227974cd52e6dbf
Amnok <ankitsingh80741@gmail.com>
2021-06-29 10:18:09 +0530

    all done

--- 6 -> rotating-navigation/app.js
+++ 6 -> rotating-navigation/app.js
@@ -3,14 +3,17 @@ const open = document.querySelector('#open');
 const close = document.querySelector('#close');
 const circleContainer = document.querySelector('.circle-container');
 const parent  = document.querySelector('.parent');
-
+const navigation = document.querySelector('.navigation');
 
 open.addEventListener('click', ()=> {
 circleContainer.style.transform = 'rotate(0.5turn)';
 parent.classList.toggle('show-nav');
+navigation.style.left = '0px';
+
 });
 
 close.addEventListener('click', ()=> {
 circleContainer.style.transform = 'rotate(0turn)';
 parent.classList.toggle('show-nav');
+navigation.style.left = '-100px';
 })
