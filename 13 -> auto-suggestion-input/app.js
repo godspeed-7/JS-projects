09d3894c2b66c88f4a0e83fbed647c929a53a834
Amnok <ankitsingh80741@gmail.com>
2021-07-10 23:44:02 +0530

    auto-suggest done

--- 13 -> auto-suggestion-input/app.js
+++ 13 -> auto-suggestion-input/app.js
@@ -1,3 +1,13 @@
-function doSomeMagic {
+const searchBox = document.querySelector('#searchBox');
+let timer = 0;
+function sendingData(e) {
+    console.log('hi', e.target.value);
+}
+function doSomething(e) {
+    clearTimeout(timer);
+    timer =  setTimeout(() => {
+       sendingData(e) 
+    }, 500);
 
 }
+searchBox.addEventListener('input', doSomething);
