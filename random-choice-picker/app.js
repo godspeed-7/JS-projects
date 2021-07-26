60a75fca9e967e5a72797beecd9ff29d65180819
Amnok <ankitsingh80741@gmail.com>
2021-07-26 22:45:39 +0530

    random-choice-picker

---
+++ random-choice-picker/app.js
@@ -0,0 +1,17 @@
+const textArea = document.querySelector('#data');
+const chipscontainer = document.querySelector('.chips');
+
+function createChip(value) {
+let button = document.createElement('button');
+button.className = 'button';
+button.innerText = value;
+chipscontainer.append(button);
+
+}
+textArea.addEventListener('input', (e)=> {
+    console.log(e);
+    if(e.data === ',') {
+let data = e.target.value.split(',');
+createChip(data[data.length - 2]);
+    }
+})
