dc8dcf61d4336fbbf701bb57701a95f0cf4e15f0
Amnok <ankitsingh80741@gmail.com>
2021-08-01 22:32:36 +0530

    drink water done

--- drink-water/app.js
+++ drink-water/app.js
@@ -1,15 +1,20 @@
 const megaContainer = document.querySelector('.jug');
 const filled = document.querySelector('.filled');
+let percent = document.querySelector('.percent');
+
+
 const incrementer = 40;
+const totValue = 320;
 let calculatedHeight = 0;
-function updateMegaContainer(e) {
+function updateMegaContainer(e) { 
   calculatedHeight += incrementer;
   filled.style.height = `${calculatedHeight}px`;
   filled.style.width = '150px';
   filled.style.backgroundColor = '#0670bb';
+  percent.textContent = `${calculatedHeight/totValue * 100}%`;
   e.target.setAttribute('data-isfilled', 'true');
 }
-const glasses = document.querySelectorAll('.glass-container');
+const glasses = document.querySelectorAll('.glass');
 glasses.forEach((glass) => {
   glass.addEventListener('click', (e) => {
     e.target.style.backgroundColor = '#0670bb';
